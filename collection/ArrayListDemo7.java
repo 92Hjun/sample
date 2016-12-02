b994863777219457462c722f68c685ad7e8084c3
Janghaeyeong <was0413@naver.com>
2016-12-02 16:57:30 +0900

    Add files via upload

--- collection/ArrayListDemo7.java
+++ collection/ArrayListDemo7.java
@@ -15,7 +15,8 @@ public class ArrayListDemo7 {
 		
 		//error: no suitable method found for sort(ArrayList<Product>)
 		// 하지만 Product 클래스에서 implements Comparable<Product> Compararble을 구현후
-		// compareTo() 메소드를 재정의하여서 양수면 오른쪽으로 , 음수면 왼쪽 0 이면 자리교체가 안된다.
+		// compareTo() 메소드를 재정의하여서 
+		// 갖고있는 번호와 다른번호를 빼서 양수면 오른쪽으로 , 음수면 왼쪽 0 이면 자리교체가 안된다.
 		// 그런후 sort를 실행하면 작은번호부터 큰번호 순으로 정렬이된다.
 
 		Collections.sort(cart);
